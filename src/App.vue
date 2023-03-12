0e894873763519393f10acc9083c1cb5d1a08285
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2023-03-12 09:37:45 +0500

    Adding Header + Total Income

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderView :totalIncome="this.state.totalIncome" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { computed } from 'vue';
+import HeaderView from './components/HeaderView.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  data() {
+    return {
+      state: {
+        income: [{
+          value: 400
+        },
+        {
+          value: 500
+        }],
+        totalIncome: computed(() => {
+          let temp = 0
+
+          if (this.state.income.length > 0) {
+            for (let i = 0; i < this.state.income.length; i++) {
+              temp += this.state.income[i].value
+            }
+          }
+
+          return temp
+        })
+      }
+    }
+  },
+  components: { HeaderView },
+  // computed: {
+  //   totalIncome: {
+  //     get() {
+  //       return this.totalIncome
+  //     },
+  //     set(value) {
+
+  //     }
+  //   }
+  // }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
+}
+
+body {
+  background: #EEE;
 }
 </style>
